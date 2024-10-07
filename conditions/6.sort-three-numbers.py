c7911e1df323505640a4adbc8b36c4ece2ea490a
srikar <srikarkanakala666@gmail.com>
2024-10-07 23:52:03 +0530

    task-1

---
+++ conditions/6.sort-three-numbers.py
@@ -0,0 +1,11 @@
+# Sort Three Numbers
+# Write a program that takes three numbers as input and prints the numbers in ascending order.
+# This program doesn't use an array.
+# Sample Input 1: 
+# 15 23 16
+# Sample Output 1: 
+# 15 16 23
+# Sample Input 2: 
+# 8 5 9
+# Sample Output 2: 
+# 5 8 9
