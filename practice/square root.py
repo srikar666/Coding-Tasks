7f2086f9d51c23b5190320eb32adb939f6b5c792
srikar <srikarkanakala666@gmail.com>
2024-10-07 23:44:21 +0530

    ver2

---
+++ practice/square root.py
@@ -0,0 +1,12 @@
+#solution-1(using Exponential)
+# num=4
+# #num1=int(input("enter a number here:"))
+# square_root=num**(1/2)
+# print("the square root of given number is",square_root)
+# exit()
+
+
+import math
+num=int(input())
+sr=math.sqrt(num)
+print(sr)
