98d803f5a091ffe07e4cae56cabd680f12dcf9ec
srikar <srikarkanakala666@gmail.com>
2024-10-08 23:01:15 +0530

    new pattern added

--- practice/patterns.py
+++ practice/patterns.py
@@ -5,3 +5,19 @@ for i in range(4):
 
   print()
   
+exit()
+
+#-------------------------------------------------
+for i in range(4):
+  
+  for i in range(i+1):
+    print("# ",end="")
+
+  print()
+  #-----------------------------------------------------
+  for i in range(4):
+
+    for j in range(4-1):
+        print("# ", end="")
+
+    print(i)
